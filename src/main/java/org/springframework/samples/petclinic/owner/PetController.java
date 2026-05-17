7fe691fa1042bc0890650433af86456d65e71502
Oshan Premkumar <opre469@aucklanduni.ac.nz>
2026-05-17 17:33:36 +1200

    refactor: removing redundant pet construction and unused parameters

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -97,9 +97,7 @@ class PetController {
 	}
 
 	@GetMapping("/pets/new")
-	public String initCreationForm(Owner owner, ModelMap model) {
-		Pet pet = new Pet();
-		owner.addPet(pet);
+	public String initCreationForm() {
 		return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
 	}
 
